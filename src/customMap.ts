3676e7d53dd404f56a254f6e96b9d11127eba2a1
Damilare <opeyemisamuel222@gmail.com>
2021-03-07 18:45:51 +0000

    using a private identify

--- src/customMap.ts
+++ src/customMap.ts
@@ -1,3 +1,14 @@
-export class customMap{
-  
+export class CustomMap{
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string){
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 14,
+        lng: 0,
+      },
+    });
+
+  }
 }
